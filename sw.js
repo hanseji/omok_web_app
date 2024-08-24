da8db8f299bb08e950860ec228229f99dfe70082
hanseji <59394775+hanseji@users.noreply.github.com>
2024-08-25 04:22:08 +0900

    reset_commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('static-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/images/icons/icon-192x192.png',
+                '/images/icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
