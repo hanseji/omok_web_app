d7c600f3aeb0da9d4501cab3f336fb70ce93ace8
hanseji <59394775+hanseji@users.noreply.github.com>
2024-08-27 06:19:34 +0900

    설치 유도

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
+let deferredPrompt;
 
-// Function to handle the shared data
-function handleSharedData() {
-    const parsedUrl = new URL(window.location);
-    const title = parsedUrl.searchParams.get('title');
-    const text = parsedUrl.searchParams.get('text');
-    const url = parsedUrl.searchParams.get('url');
+window.addEventListener('beforeinstallprompt', (e) => {
+    // 설치 프롬프트를 보여주기 전에 발생하는 이벤트를 차단
+    e.preventDefault();
+    deferredPrompt = e;
 
-    if (text) {
-        document.getElementById('message').innerText = `Received YouTube link: ${text}`;
-    }
-}
+    // 설치 버튼 보여주기
+    const installButton = document.getElementById('installBtn');
+    installButton.style.display = 'block';
 
-function askForNPerm() {
-    Notification.requestPermission().then(function(result) {
-        console.log("User choice", result);
-        if (result == "granted") {
-            console.log("권한 허용됨");
-            //configurePushSub();// Write your custom function that pushes your message
-        } else {
-            console.log("No notification permission granted!");
-        }
+    installButton.addEventListener('click', (e) => {
+        // 버튼 클릭 시 설치 프롬프트 보여주기
+        deferredPrompt.prompt();
+        // 사용자가 설치를 결정했는지 확인
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
     });
-}
+});
+
 
 window.addEventListener('DOMContentLoaded', () => {
     document.body.style.display = 'block'; // 문서가 준비되면 본문 보이기
-    //askForNPerm()
-    handleSharedData();
 });
