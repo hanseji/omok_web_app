002ff7fcb5c8b74ad919cd18f07157bf7f7de54a
hanseji <59394775+hanseji@users.noreply.github.com>
2024-08-27 05:26:17 +0900

    돌아가기 버튼 링크 수정

--- js/view-results-page.js
+++ js/view-results-page.js
@@ -125,5 +125,5 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 document.getElementById('closeApp').addEventListener('click', function () {
-    window.open('https://youtu.be/watch?v=CAtxNQDUzvk', '_blank');
+    window.open(`https://youtu.be/watch?v=${videoId}`, '_blank');
 });
