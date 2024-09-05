bbd623de83a63926e19db0d46839aee6529a03a8
hanseji <59394775+hanseji@users.noreply.github.com>
2024-09-06 00:59:42 +0900

    standalone 오류 해결

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
 import { setCookie, getCookie, deleteCookie, isUserLogin } from "./utility.js";
 import { checkClipboardForYoutubeLink } from './clipboardModule.js';
 
-var userMode;
-
-handleSharedData();
-
-console.log(userMode == "app");
 
 //처음 앱을 실행할 때(웹이 아닌 앱 환경으로)
 console.log(('standalone' in window.navigator) );
 console.log(window.matchMedia('(display-mode: standalone)').matches);
 console.log(window.navigator.standalone);
 console.log(getCookie('isStart') == null);
-if (window.matchMedia('(display-mode: standalone)').matches && getCookie('isStart') == null) {
+if (isInStandaloneMode() && getCookie('isStart') == null) {
     //튜토리얼 시작
     location.href = "tutorial.html";
 }
@@ -31,13 +26,6 @@ const loginButton = document.getElementById('loginButton');
 const giveFeeback = document.getElementById('giveFeedback');
 const logoutButton = document.getElementById('logoutButton');
 const tutorialButton = document.getElementById('tutorialButton');
-
-
-// Function to handle the shared data
-function handleSharedData() {
-    const parsedUrl = new URL(window.location);
-    userMode = String(parsedUrl.searchParams.get('userMode')) || "null";
-}
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent the mini-infobar from appearing on mobile
@@ -120,7 +108,7 @@ function isIos() {
 }
 
 function isInStandaloneMode() {
-    return ('standalone' in window.navigator) || (window.navigator.standalone);
+    return window.matchMedia('(display-mode: standalone)').matches || (window.navigator.standalone);
 }
 
 window.onload = () => {
